d059ca2de6815f86ba65a10142ba94eb9669df55
angela4713 <ang3l_gu@hotmail.com>
2014-11-10 17:05:43 -0500

    update test

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,7 +11,6 @@ describe "Authentication" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
